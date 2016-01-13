c3bfb862432ca0fdb8ea9275f9c7340d5d5787a7
Dian Pan <dian.di.pan@gmail.com>
2016-01-13 11:50:46 -0800

    edit pdf doc content

--- app/pdfs/doc_pdf.rb
+++ app/pdfs/doc_pdf.rb
@@ -4,15 +4,20 @@ class DocPdf < Prawn::Document
     text "This is your loan document"
     @document = document
     @view = view
-    text "Document #{@document.id}"
+    loan_amount
+    down_payment
+    interest_rate
   end
 
   def loan_amount
+    text "Loan amount: $#{@document.loan_amount}"
   end
 
   def down_payment
+    text "Down payment: $#{@document.down_payment}"
   end
 
   def interest_rate
+    text "Interest rate: %#{@document.interest_rate}"
   end
 end
