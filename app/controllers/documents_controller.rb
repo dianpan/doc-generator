849dfcf733e099e5a85b6bef4e7c84fe114dda54
Dian Pan <dian.di.pan@gmail.com>
2016-01-12 22:12:42 -0800

    add authentication to documents controller

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,12 +1,12 @@
 class DocumentsController < ApplicationController
-  before_action :find_user
+  before_action :authenticate_user!
 
   def new
-    @document = Documents.new
+    @document = current_user.documents.new
   end
 
   def create
-    document = @user.documents.build(documents_params)
+    document = current_user.documents.build(documents_params)
     document.save ? flash[:message] = "Success, document has been created!" : flash[:error] = "Document did not save, please try again."
     redirect_to root_path
   end
@@ -14,9 +14,5 @@ class DocumentsController < ApplicationController
   private
     def documents_params
       params.require(:document).permit(:loan_amount, :down_payment, :interest_rate)
-    end
-
-    def find_user
-      @user = User.find(session[:user_id])
     end
 end
