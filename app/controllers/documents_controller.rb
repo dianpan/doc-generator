c4f4676ce53ae899b7ebfd8177f16d74fe6120d2
Dian Pan <dian.di.pan@gmail.com>
2016-01-12 21:47:51 -0800

    update document controller logic

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,10 +1,22 @@
 class DocumentsController < ApplicationController
+  before_action :find_user
+
   def new
-    @document = current_user.documents.new
+    @document = Documents.new
+  end
+
+  def create
+    document = @user.documents.build(documents_params)
+    document.save ? flash[:message] = "Success, document has been created!" : flash[:error] = "Document did not save, please try again."
+    redirect_to root_path
   end
 
   private
-  def documents_params
-    params.require(:document).permit(:)
-  end
+    def documents_params
+      params.require(:document).permit(:loan_amount, :down_payment, :interest_rate)
+    end
+
+    def find_user
+      @user = User.find(session[:user_id])
+    end
 end
