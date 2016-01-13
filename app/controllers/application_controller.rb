8024d395553b8afb29f5ec6cb074e9afa76be30a
Dian Pan <dian.di.pan@gmail.com>
2016-01-12 21:38:44 -0800

    add documents controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user
+    if logged_in?
+      @user ||= User.find(session[:user_id])
+    end
+  end
+
 end
--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,3 +1,10 @@
 class DocumentsController < ApplicationController
+  def new
+    @document = current_user.documents.new
+  end
 
+  private
+  def documents_params
+    params.require(:document).permit(:)
+  end
 end
