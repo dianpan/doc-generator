527b0b67b8f786fd08ea29d043223453108b4319
Dian Pan <dian.di.pan@gmail.com>
2016-01-13 11:44:44 -0800

    add pdf inline view

--- app/pdfs/doc_pdf.rb
+++ app/pdfs/doc_pdf.rb
@@ -2,5 +2,17 @@ class DocPdf < Prawn::Document
   def initialize(document, view)
     super()
     text "This is your loan document"
+    @document = document
+    @view = view
+    text "Document #{@document.id}"
+  end
+
+  def loan_amount
+  end
+
+  def down_payment
+  end
+
+  def interest_rate
   end
 end
