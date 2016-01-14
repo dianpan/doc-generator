5f317bec9a317806b7900dfd47b270fd43b11e16
Dian Pan <dian.di.pan@gmail.com>
2016-01-14 14:45:53 -0800

    override devise session redirect path

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   protected
     def after_sign_up_path_for(resource)
-      root_path
+      documents_path
     end
     def after_update_path_for(resource)
-      root_path
+      documents_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {registrations: 'users/registrations'}
+  devise_for :users, controllers: {registrations: 'users/registrations', sessions: 'users/sessions'}
   resources :documents
-  root 'documents#index'
+  # root 'documents#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
