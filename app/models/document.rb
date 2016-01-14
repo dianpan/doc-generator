9862838d879c70405865e32e598b0fe9ea2d0130
Dian Pan <dian.di.pan@gmail.com>
2016-01-14 14:55:48 -0800

    add file validations in doc model

--- app/models/document.rb
+++ app/models/document.rb
@@ -5,4 +5,8 @@ class Document < ActiveRecord::Base
   validates_numericality_of :loan_amount, :greater_than => 0
 
   belongs_to :user
+
+  has_attached_file :document
+  validates_attachment :document, :content_type => { :content_type => 'application/pdf' }
+
 end
