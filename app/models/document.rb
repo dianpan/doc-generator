fdf45bbbf5bf5b76e6ea7d2e60877643a0489e94
Dian Pan <dian.di.pan@gmail.com>
2016-01-14 21:41:26 -0800

    add stronger validations for document

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,9 +2,10 @@ class Document < ActiveRecord::Base
   validates_presence_of :loan_amount
   validates_presence_of :down_payment
   validates_presence_of :interest_rate
-  validates_numericality_of :loan_amount, :greater_than => 0
-  validates_numericality_of :down_payment, :greater_than_or_equal_to => 0
-  validates_numericality_of :interest_rate, :greater_than_or_equal_to => 0
+
+  validates_numericality_of :loan_amount, :greater_than => 0, on: :create
+  validates_numericality_of :down_payment, :greater_than_or_equal_to => 0, on: :create
+  validates_numericality_of :interest_rate, :greater_than_or_equal_to => 0, inclusion: { in: 0..100 }, on: :create
 
   belongs_to :user
 
