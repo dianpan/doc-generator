638780814f4c62a5ca6e8d907a9a0fcfcac3dc8d
Dian Pan <dian.di.pan@gmail.com>
2016-01-14 18:29:14 -0800

    update doc controller to save pdfs and gen url

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -13,29 +13,30 @@ class DocumentsController < ApplicationController
     document = Document.new(documents_params)
     document.user_id = current_user.id
     pdf = DocPdf.new(document, view_context)
-    pdf.render_file("loan_document.pdf")
-    pdf_file = File.open("loan_document.pdf")
-    document.file = pdf_file
+    file_name = "loan_document_#{current_user.id}.pdf"
+    pdf.render_file(file_name)
+    document.file = File.open(file_name)
+    url =  "#{document.file}"
 
     document.save! ? flash[:success] = "Success, document has been created!" : flash[:error] = "Document did not save, please try again."
-      redirect_to documents_path
+    redirect_to documents_path
   end
 
-  def show
-    @document = Document.find(params[:id])
+  # def show
+  #   @document = Document.find(params[:id])
 
-    respond_to do |format|
-      format.html
-      format.pdf do
-        pdf = DocPdf.new(@document, view_context)
-        send_data pdf.render, filename:
-        "document_#{@document.created_at.strftime("%d/%m/%Y")}.pdf",
-        type: "application/pdf",
-        disposition: "inline"
-      end
-    end
+  #   respond_to do |format|
+  #     format.html
+  #     format.pdf do
+  #       pdf = DocPdf.new(@document, view_context)
+  #       send_data pdf.render, filename:
+  #       "document_#{@document.created_at.strftime("%d/%m/%Y")}.pdf",
+  #       type: "application/pdf",
+  #       disposition: "inline"
+  #     end
+  #   end
 
-  end
+  # end
 
   private
     def documents_params
