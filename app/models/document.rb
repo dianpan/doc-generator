0f84040b85978280c2bd24f0f8e7102e06fcf543
Dian Pan <dian.di.pan@gmail.com>
2016-01-14 19:36:51 -0800

    remove comments

--- app/models/document.rb
+++ app/models/document.rb
@@ -10,7 +10,3 @@ class Document < ActiveRecord::Base
   validates_attachment :file, :content_type => { :content_type => 'application/pdf' }
 
 end
-
-
-http://s3.amazonaws.com/roostifyfile/documents/files/000/000/007/original/loan_document_2.pdf?1452828710
-https://s3-us-west-1.amazonaws.com/roostifyfile/documents/files/000/000/006/original/loan_document_2.pdf
