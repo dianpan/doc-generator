422a51809fefd198715007a4218b295731e95d45
Dian Pan <dian.di.pan@gmail.com>
2016-01-14 21:32:18 -0800

    add document db validations

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,6 +3,8 @@ class Document < ActiveRecord::Base
   validates_presence_of :down_payment
   validates_presence_of :interest_rate
   validates_numericality_of :loan_amount, :greater_than => 0
+  validates_numericality_of :down_payment, :greater_than_or_equal_to => 0
+  validates_numericality_of :interest_rate, :greater_than_or_equal_to => 0
 
   belongs_to :user
 
