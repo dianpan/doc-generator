d3b8b0038c75e9887b5e5420974c07b9dde19080
Dian Pan <dian.di.pan@gmail.com>
2016-01-16 22:46:57 -0800

    add more validations on edge cases

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,7 @@
 class Document < ActiveRecord::Base
+  belongs_to :user
+  before_save :round_interest_rate
+
   validates_presence_of :loan_amount, message: "Please fill in a loan amount."
   validates_presence_of :down_payment, message: "Please fill in a down payment amount."
   validates_presence_of :interest_rate, message: "Please fill in your interest rate."
@@ -7,9 +10,19 @@ class Document < ActiveRecord::Base
   validates_numericality_of :down_payment, :greater_than_or_equal_to => 0, on: :create, message: "Sorry, down payment cannot be negative!"
   validates_numericality_of :interest_rate, :greater_than_or_equal_to => 0, inclusion: { in: 0..100 }, on: :create, message: "Nice try, but interest rates can't be negative!"
 
-  belongs_to :user
+  validate :downpay_cannot_be_greater_than_loan
 
   has_attached_file :file
   validates_attachment :file, :content_type => { :content_type => 'application/pdf' }
+
+  def downpay_cannot_be_greater_than_loan
+    if down_payment > loan_amount
+      errors.add(:down_payment, "cannot be greater than your loan amount")
+    end
+  end
+
+  def round_interest_rate
+    interest_rate.round
+  end
 
 end
