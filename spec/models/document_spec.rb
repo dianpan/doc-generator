82e5bbd4de1c6e3a96a65e97bbb5c63ca335fcc8
Dian Pan <dian.di.pan@gmail.com>
2016-01-16 22:18:33 -0800

    add document model tests

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,7 +1,40 @@
+require 'rails_helper'
+
 describe Document do
-  it 'is valid with a loan amount, down payment, interest rate, and file attachment'
-  it 'is invalid with a loan amount of $0'
-  it 'is invalid with a negative down payment amount'
-  it 'is invalid with a negative down interest rate'
-  it 'is invalid with a negative down interest rate'
+  it 'is valid with a loan amount, down payment, interest rate, and file attachment' do
+    document = Document.new(
+      loan_amount: 10000,
+      down_payment: 100,
+      interest_rate: 10)
+    expect(document).to be_valid
+  end
+
+  it 'is invalid with a loan amount of $0' do
+    document = Document.new(loan_amount: 0)
+    expect(document.valid?).to eq(false)
+  end
+
+  it 'is invalid with a negative loan amount' do
+    document = Document.new(
+      loan_amount: -1,
+      down_payment: 0,
+      interest_rate: 0)
+    expect(document.valid?).to eq(false)
+  end
+
+  it 'is invalid with a negative down payment amount' do
+    document = Document.new(
+      loan_amount: 100,
+      down_payment: -1,
+      interest_rate: 10)
+    expect(document.valid?).to eq(false)
+  end
+
+  it 'is invalid with a negative down interest rate' do
+    document = Document.new(
+      loan_amount: 100,
+      down_payment: 10,
+      interest_rate: -1)
+    expect(document.valid?).to eq(false)
+  end
 end
