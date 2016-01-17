2854dce435507b5cf237cb33dd519c0cec0923e4
Dian Pan <dian.di.pan@gmail.com>
2016-01-16 21:55:08 -0800

    add document model test shell

---
+++ spec/models/document_spec.rb
@@ -0,0 +1,7 @@
+describe Document do
+  it 'is valid with a loan amount, down payment, interest rate, and file attachment'
+  it 'is invalid with a loan amount of $0'
+  it 'is invalid with a negative down payment amount'
+  it 'is invalid with a negative down interest rate'
+  it 'is invalid with a negative down interest rate'
+end
