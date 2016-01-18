f2f0a1e1275a1b03a471c9e88659e33e506956a9
Dian Pan <dian.di.pan@gmail.com>
2016-01-18 11:13:31 -0800

    add doc model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 describe User do
-
+  it 'is valid with an email and pw' do
+    user = User.new(email: "dian@example.com", password: "12345678")
+    expect(user).to be_valid
+  end
 end
