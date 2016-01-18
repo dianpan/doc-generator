246fd358bc5edebcc765733108c36bdabb7f34ce
Dian Pan <dian.di.pan@gmail.com>
2016-01-18 12:00:38 -0800

    add homepage styling

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,6 @@
 require 'rails_helper'
 
-describe User, type: :model do
-  # context 'contains valid data' do
-  #   it { expect(subject).to validate_presence_of(:email) }
-  #   it { expect(subject).to validate_presence_of(:password).on(:create) }
-  #   it { expect(subject).to validate_length_of(:password).is_at_least(8).on(:create) }
-  #   it { expect(FactoryGirl.build(:user)).to validate_presence_of(:email).on(:save) }
-  # end
-
+describe User do
   context 'contains valid data' do
     it 'is valid with an email and pw' do
       user = User.new(email: "dian@example.com", password: "12345678")
